05dd6ba52cde9e7a021289894a34ea128458803e
Helen Jiang <helen.jiang@hashicorp.com>
2024-11-14 10:36:56 -0800

    oops

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "2024-11-14-helen-demo" {
+resource "aws_s3_bucket" "helen-demo-2024-11-14" {
   bucket = "2024-11-14-helen-demo"
 }
